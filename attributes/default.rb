a71ab82a3b6d77b4def1cd02aeebe75d90769a9d
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-09-24 22:19:49 +0900

    rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,5 +9,5 @@ default['graphite']['password'] = 'password'
 default['graphite']['timezone'] = 'Asia/Tokyo'
 default['graphite']['apache']['basic_auth']['enabled'] = true
 default['graphite']['apache']['basic_auth']['file_path'] = "#{node['graphite']['doc_root']}/htpasswd"
-default['graphite']['apache']['basic_auth']['user'] = "admin"
-default['graphite']['apache']['basic_auth']['pass'] = "password"
+default['graphite']['apache']['basic_auth']['user'] = 'admin'
+default['graphite']['apache']['basic_auth']['pass'] = 'password'
